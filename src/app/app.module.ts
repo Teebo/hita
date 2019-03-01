9be896a0f1ff3ebe0f776d24a3b97dd3c81f68ec
Thabo <ngubanethabo.ambrose@gmail.com>
2019-03-01 15:20:38 +0200

    Revert change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
 import { ComponentsModule } from './components/components.module';
 
 const config: SocketIoConfig = {
-  url: 'http://localhost:3026',
+  url: 'https://hita-app.herokuapp.com',
   options: {}
 };
 
